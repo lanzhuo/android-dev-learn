5b09ca18d4cb5bdc21127b659eb9398de25e544e
lanzhuo2011 <lanzhuo_2008@126.com>
2018-01-27 21:14:04 +0800

    base ui 1

---
+++ base-ui/app/src/main/java/com/example/lan/baseui1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.lan.baseui1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+
+/**
+ * Created by Lan on 2018/1/27.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        ImageView leftBack = (ImageView)findViewById(R.id.image_left_back);
+        leftBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
